a7480dff3efadc877ae995369753eacb765916cb
LEE YOON SEOK <dmzld94@gmail.com>
2018-12-08 20:27:55 +0900

    connect db in freeBoard

--- app/src/main/java/com/example/user/armonia/activity/FreeBoardActivity.java
+++ app/src/main/java/com/example/user/armonia/activity/FreeBoardActivity.java
@@ -1,6 +1,7 @@
 package com.example.user.armonia.activity;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -12,7 +13,16 @@ import com.example.user.armonia.R;
 import com.example.user.armonia.adapter.AdapterListPost;
 import com.example.user.armonia.list.ListPost;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class FreeBoardActivity extends AppCompatActivity {
 
@@ -21,25 +31,36 @@ public class FreeBoardActivity extends AppCompatActivity {
     ArrayList<ListPost> freeArrayList;
     Button btnWrite;
 
+    //server
+    String myJSON;
+    private static final String TAG_RESULTS = "result";
+    private static final String TAG_title = "Title";
+    private static final String TAG_name = "User";
+    private static final String TAG_date = "Date";
+    JSONArray list = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_free_board);
 
+        //intent 받은거 받아야함
+        Intent intent = getIntent();
+
+
         //전체 자유게시판
         listFreeView = (ListView)findViewById(R.id.listFreeView);
         freeArrayList = new ArrayList<ListPost>();
+
+
         btnWrite = (Button)findViewById(R.id.btnWrite);
-
-        // 아직 DB 연동 안함
-        freeArrayList.add(new ListPost("1번째 글","이주형","2018/12/05"));
-        freeArrayList.add(new ListPost("2번째 글","이주형","2018/12/05"));
-        freeArrayList.add(new ListPost("3번째 글","이주형","2018/12/05"));
-        freeArrayList.add(new ListPost("4번째 글","이주형","2018/12/05"));
-        freeArrayList.add(new ListPost("5번째 글","이주형","2018/12/05"));
-
-        adapterListFree = new AdapterListPost(getApplicationContext(),freeArrayList);
-        listFreeView.setAdapter(adapterListFree);
+        btnWrite.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),WritePostActivity.class);
+                startActivity(intent);
+            }
+        });
 
         listFreeView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
@@ -50,5 +71,67 @@ public class FreeBoardActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+
+        //주소
+        getData("http://ec2-52-79-235-82.ap-northeast-2.compute.amazonaws.com/armonia/freeBoard.php");
+    }
+
+    protected void showList() {
+        try {
+            JSONObject jsonObj = new JSONObject(myJSON);
+            list = jsonObj.getJSONArray(TAG_RESULTS);
+
+            for (int i = 0; i < list.length(); i++) {
+                JSONObject c = list.getJSONObject(i);
+                String title = c.getString(TAG_title);
+                String name = c.getString(TAG_name);
+                String date = c.getString(TAG_date);
+                ListPost freePost = new ListPost(title,name,date);
+                freeArrayList.add(freePost);
+            }
+            Collections.reverse(freeArrayList);
+            adapterListFree = new AdapterListPost(FreeBoardActivity.this,freeArrayList);
+            listFreeView.setAdapter(adapterListFree);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void getData(String url) {
+        class GetDataJSON extends AsyncTask<String, Void, String> {
+
+            @Override
+            protected String doInBackground(String... params) {
+
+                String uri = params[0];
+
+                BufferedReader bufferedReader = null;
+                try {
+                    URL url = new URL(uri);
+                    HttpURLConnection con = (HttpURLConnection) url.openConnection();
+                    StringBuilder sb = new StringBuilder();
+
+                    bufferedReader = new BufferedReader(new InputStreamReader(con.getInputStream()));
+
+                    String json;
+                    while ((json = bufferedReader.readLine()) != null) {
+                        sb.append(json + "\n");
+                    }
+
+                    return sb.toString().trim();
+
+                } catch (Exception e) {
+                    return null;
+                }
+            }
+            @Override
+            protected void onPostExecute(String result) {
+                myJSON = result;
+                showList();
+            }
+        }
+        GetDataJSON g = new GetDataJSON();
+        g.execute(url);
     }
 }
--- app/src/main/java/com/example/user/armonia/fragment/ClubBoardFrag.java
+++ app/src/main/java/com/example/user/armonia/fragment/ClubBoardFrag.java
@@ -1,6 +1,7 @@
 package com.example.user.armonia.fragment;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -10,13 +11,23 @@ import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListView;
 
+import com.example.user.armonia.activity.FreeBoardActivity;
 import com.example.user.armonia.activity.PostActivity;
 import com.example.user.armonia.activity.WritePostActivity;
 import com.example.user.armonia.list.ListPost;
 import com.example.user.armonia.R;
 import com.example.user.armonia.adapter.AdapterListPost;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class ClubBoardFrag extends Fragment {
 
@@ -27,6 +38,14 @@ public class ClubBoardFrag extends Fragment {
     ListView listPostView;
     AdapterListPost adapterListPost;
     ArrayList<ListPost> postArrayList;
+
+    //server
+    String myJSON;
+    private static final String TAG_RESULTS = "result";
+    private static final String TAG_title = "Title";
+    private static final String TAG_name = "User";
+    private static final String TAG_date = "Date";
+    JSONArray list = null;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -50,19 +69,17 @@ public class ClubBoardFrag extends Fragment {
         });
 
 
-        // 아직 DB 연동 안함
-
-        postArrayList.add(new ListPost("1번째 글","이주형","2018/12/05"));
-        postArrayList.add(new ListPost("2번째 글","이주형","2018/12/05"));
-        postArrayList.add(new ListPost("3번째 글","이주형","2018/12/05"));
-        postArrayList.add(new ListPost("4번째 글","이주형","2018/12/05"));
-        postArrayList.add(new ListPost("5번째 글","이주형","2018/12/05"));
-        postArrayList.add(new ListPost("6번째 글","이주형","2018/12/05"));
-        postArrayList.add(new ListPost("7번째 글","이주형","2018/12/05"));
-        postArrayList.add(new ListPost("8번째 글","이주형","2018/12/05"));
-
-        adapterListPost = new AdapterListPost(getActivity(),postArrayList);
-        listPostView.setAdapter(adapterListPost);
+//        // 아직 DB 연동 안함
+//        postArrayList.add(new ListPost("1번째 글","이주형","2018/12/05"));
+//        postArrayList.add(new ListPost("2번째 글","이주형","2018/12/05"));
+//        postArrayList.add(new ListPost("3번째 글","이주형","2018/12/05"));
+//        postArrayList.add(new ListPost("4번째 글","이주형","2018/12/05"));
+//        postArrayList.add(new ListPost("5번째 글","이주형","2018/12/05"));
+//        postArrayList.add(new ListPost("6번째 글","이주형","2018/12/05"));
+//        postArrayList.add(new ListPost("7번째 글","이주형","2018/12/05"));
+//        postArrayList.add(new ListPost("8번째 글","이주형","2018/12/05"));
+//        adapterListPost = new AdapterListPost(getActivity(),postArrayList);
+//        listPostView.setAdapter(adapterListPost);
 
         listPostView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
@@ -75,7 +92,68 @@ public class ClubBoardFrag extends Fragment {
             }
         });
 
+        //주소
+       // getData("http://ec2-52-79-235-82.ap-northeast-2.compute.amazonaws.com/armonia/clubBoard.php");
+
         return view;
     }
+
+//    protected void showList() {
+//        try {
+//            JSONObject jsonObj = new JSONObject(myJSON);
+//            list = jsonObj.getJSONArray(TAG_RESULTS);
+//
+//            for (int i = 0; i < list.length(); i++) {
+//                JSONObject c = list.getJSONObject(i);
+//                String title = c.getString(TAG_title);
+//                String name = c.getString(TAG_name);
+//                String date = c.getString(TAG_date);
+//                ListPost freePost = new ListPost(title,name,date);
+//                postArrayList.add(clubPost);
+//            }
+//            Collections.reverse(freeArrayList);
+//            adapterListFree = new AdapterListPost(FreeBoardActivity.this,freeArrayList);
+//            listFreeView.setAdapter(adapterListFree);
+//        } catch (JSONException e) {
+//            e.printStackTrace();
+//        }
+//    }
+//
+//    public void getData(String url) {
+//        class GetDataJSON extends AsyncTask<String, Void, String> {
+//
+//            @Override
+//            protected String doInBackground(String... params) {
+//
+//                String uri = params[0];
+//
+//                BufferedReader bufferedReader = null;
+//                try {
+//                    URL url = new URL(uri);
+//                    HttpURLConnection con = (HttpURLConnection) url.openConnection();
+//                    StringBuilder sb = new StringBuilder();
+//
+//                    bufferedReader = new BufferedReader(new InputStreamReader(con.getInputStream()));
+//
+//                    String json;
+//                    while ((json = bufferedReader.readLine()) != null) {
+//                        sb.append(json + "\n");
+//                    }
+//
+//                    return sb.toString().trim();
+//
+//                } catch (Exception e) {
+//                    return null;
+//                }
+//            }
+//            @Override
+//            protected void onPostExecute(String result) {
+//                myJSON = result;
+//                showList();
+//            }
+//        }
+//        GetDataJSON g = new GetDataJSON();
+//        g.execute(url);
+//    }
 
 }
