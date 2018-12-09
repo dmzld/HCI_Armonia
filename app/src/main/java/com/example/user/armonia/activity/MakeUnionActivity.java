4bb603b8ba101827c58a168fb3b9a15c5531d9eb
cheolwon1994 <cheolwon1994>
2018-12-09 17:55:01 +0900

    연합생성, 동아리 생성 디비연결

--- app/src/main/java/com/example/user/armonia/activity/MakeUnionActivity.java
+++ app/src/main/java/com/example/user/armonia/activity/MakeUnionActivity.java
@@ -1,6 +1,9 @@
 package com.example.user.armonia.activity;
 
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.AdapterView;
@@ -13,21 +16,36 @@ import android.widget.Toast;
 
 import com.example.user.armonia.R;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
+import java.util.ArrayList;
+
 public class MakeUnionActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
 
     TextView selectedText;
     Spinner spinner;        //내가 속한 동아리목록
-    Spinner spinner2;       //연합하고자 하는 동아리목록
     String[] item;          //spinner 1 목록
-    String[] item2;         //spinner 2 목록
+    String myJSON;
 
-    //아직 전부는 구현안함
+    private String cu;          //동아리/연합 구분 위해 사용
     private EditText union_name;
-//    private Button btnAdd;
+    private EditText president_name;
     private EditText union_content;
     private Button btn_make_union;
     private Button btn_cancel;
 
+    //spin값 받아오기 위해 사용
+    private String spin;
+    JSONArray list = null;
 
 
     @Override
@@ -35,34 +53,30 @@ public class MakeUnionActivity extends AppCompatActivity implements AdapterView.
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_make_union);
 
+        Intent intent = getIntent();
+        cu = intent.getStringExtra("cu");
+
         union_name = (EditText)findViewById(R.id.union_name);
+        president_name = (EditText)findViewById(R.id.president_name);
         spinner = (Spinner)findViewById(R.id.my_club);
-        spinner2 = (Spinner)findViewById(R.id.add_union);
         union_content = (EditText)findViewById(R.id.union_content);
         btn_make_union = (Button)findViewById(R.id.btn_make_union);
         btn_cancel = (Button)findViewById(R.id.btn_cancel);
-//        btnAdd = (Button)findViewById(R.id.btnAdd);
 
-        //spinner에 나타날 인자(?)들
+
+        //********수정필요******spinner에 나타날 인자(?)들
         item = new String[]{"선택하세요","볼랜드","한터","5분쉼표","학생회","안시"};
-        item2 = new String[]{"선택하세요","소울","혜윰","총학","ABBA"};
 
         //spinner생성 코드
         ArrayAdapter <String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
 
-        ArrayAdapter <String> adapter2 = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,item2);
-        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner2.setAdapter(adapter2);
+
+  //      getData("http://52.11.180.128/dbProject/6_1.php","");
 
 
-        //*****추가해야됨****
-//        btnAdd.setOnClickListener(new View.OnClickListener() {
-//            public void onClick(View v) {
-//
-//            }
-//        });
+
 
         btn_make_union.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -71,18 +85,28 @@ public class MakeUnionActivity extends AppCompatActivity implements AdapterView.
                     Toast.makeText(MakeUnionActivity.this, "연합명을 입력하세요!", Toast.LENGTH_SHORT).show();
                     return;
                 }
-                else if(spinner.getSelectedItem().toString().equals("선택하세요")){
+
+                if(president_name.getText().toString().length()==0){
+                    Toast.makeText(MakeUnionActivity.this, "회장의 학번을 입력하세요!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if(spinner.getSelectedItem().toString().equals("선택하세요")){
                     Toast.makeText(MakeUnionActivity.this, "소속 동아리를 입력하세요!", Toast.LENGTH_SHORT).show();
                     return;
                 }
-                else if(spinner2.getSelectedItem().toString().equals("선택하세요")){
-                    Toast.makeText(MakeUnionActivity.this, "연합할 동아리명을 입력하세요!", Toast.LENGTH_SHORT).show();
-                    return;
-                }
-                else if(union_content.getText().toString().length()==0){
+
+                if(union_content.getText().toString().length()==0){
                     Toast.makeText(MakeUnionActivity.this, "연합 설명을 입력하세요!", Toast.LENGTH_SHORT).show();
                     return;
                 }
+
+                spin = spinner.getSelectedItem().toString();
+
+                //함수로 전달
+                showData(union_name.getText().toString(), spin, union_content.getText().toString(), president_name.getText().toString(),cu);
+                finish();
+
             }
         });
 
@@ -95,7 +119,126 @@ public class MakeUnionActivity extends AppCompatActivity implements AdapterView.
         });
     }
 
-    //나중에 수정야함
+//
+//    public void getData(String url, String category) {
+//        class GetDataJSON extends AsyncTask<String, Void, String> {
+//
+//            @Override
+//            protected String doInBackground(String... params) {
+//
+//
+//                BufferedReader bufferedReader = null;
+//                try {
+//                    String uri = params[0];
+//                    String category = params[1];
+//                    String data = URLEncoder.encode("category","UTF-8")+"="+URLEncoder.encode(category,"UTF-8");
+//                    URL url = new URL(uri);
+//                    HttpURLConnection con = (HttpURLConnection) url.openConnection();
+//
+//                    con.setDoOutput(true);
+//                    OutputStreamWriter wr = new OutputStreamWriter(con.getOutputStream());
+//
+//                    wr.write(data);
+//                    wr.flush();
+//
+//                    StringBuilder sb = new StringBuilder();
+//
+//                    bufferedReader = new BufferedReader(new InputStreamReader(con.getInputStream()));
+//
+//                    String json;
+//                    while ((json = bufferedReader.readLine()) != null) {
+//                        sb.append(json + "\n");
+//                    }
+//                    Log.i("after","after");
+//
+//                    return sb.toString().trim();
+//
+//                } catch (Exception e) {
+//                    return null;
+//                }
+//
+//
+//            }
+//
+//            @Override
+//            protected void onPostExecute(String result) {
+//                myJSON = result;
+//            }
+//        }
+//        GetDataJSON g = new GetDataJSON();
+//        g.execute(url,category);
+//    }
+
+
+    //디비와 연동하는 함수
+    public void showData(String union_name, String spin, String union_content, String president_name, String cu){
+
+        class InsetData extends AsyncTask<String, Void, String> {
+
+            @Override
+            protected void onPreExecute(){
+                super.onPreExecute();
+            }
+
+            @Override
+            protected void onPostExecute(String s){
+                super.onPostExecute(s);
+                Toast.makeText(getApplicationContext(),s,Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            protected  String doInBackground(String... params)
+            {
+                try{
+                    String union_name = (String)params[0];
+                    String spin = (String)params[1];
+                    String union_content = (String)params[2];
+                    String president_name = (String)params[3];
+                    String cu = (String)params[4];
+
+
+                    String link = "http://ec2-52-79-235-82.ap-northeast-2.compute.amazonaws.com/armonia/makeUnion.php";
+
+                    String data = URLEncoder.encode("union_name","UTF-8")+"="+URLEncoder.encode(union_name,"UTF-8");
+                    data += "&" + URLEncoder.encode("spin","UTF-8")+"="+URLEncoder.encode(spin,"UTF-8");
+                    data += "&" + URLEncoder.encode("union_content","UTF-8")+"="+URLEncoder.encode(union_content,"UTF-8");
+                    data += "&" + URLEncoder.encode("president_name","UTF-8")+"="+URLEncoder.encode(president_name,"UTF-8");
+                    data += "&" + URLEncoder.encode("cu","UTF-8")+"="+URLEncoder.encode(cu,"UTF-8");
+
+                    URL url = new URL(link);
+                    URLConnection conn = url.openConnection();
+
+                    conn.setDoOutput(true);
+                    OutputStreamWriter wr = new OutputStreamWriter(conn.getOutputStream());
+
+                    wr.write(data);
+                    wr.flush();
+
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+
+                    StringBuilder sb = new StringBuilder();
+                    String line = null;
+
+                    while((line = reader.readLine()) != null)
+                    {
+                        sb.append(line);
+                        break;
+                    }
+                    return sb.toString();
+                }
+                catch(Exception e)
+                {
+                    return new String("Exception: "+e.getMessage());
+                }
+            }
+        }
+
+        InsetData task = new InsetData();
+        task.execute(union_name,spin,union_content,president_name,cu);
+    }
+
+
+
     @Override
     public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l){
         selectedText.setText(item[i]);
@@ -103,7 +246,7 @@ public class MakeUnionActivity extends AppCompatActivity implements AdapterView.
             selectedText.setText("");
         }
     }
-    //이것 역시 수정해야함
+
     @Override
     public void onNothingSelected(AdapterView<?> adapterView){
         selectedText.setText("");
